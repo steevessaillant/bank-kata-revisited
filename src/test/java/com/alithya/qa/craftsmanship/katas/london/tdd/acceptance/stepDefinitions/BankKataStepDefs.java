1c6aa1a32bfb9e4fe6da7f6a14785b716050bcb3
Steeves Saillant <steeves.saillant@alithya.com>
2021-12-07 17:08:20 -0500

    -1st commit - Starting point - Pending Specs.

--- src/test/java/com/alithya/qa/craftsmanship/katas/london/tdd/acceptance/stepDefinitions/BankKataStepDefs.java
+++ src/test/java/com/alithya/qa/craftsmanship/katas/london/tdd/acceptance/stepDefinitions/BankKataStepDefs.java
@@ -1,28 +1,16 @@
 package com.alithya.qa.craftsmanship.katas.london.tdd.acceptance.stepDefinitions;
 
-import com.alithya.qa.craftsmanship.katas.london.tdd.Account;
-import com.alithya.qa.craftsmanship.katas.london.tdd.Amount;
-import com.alithya.qa.craftsmanship.katas.london.tdd.Statement;
-import com.alithya.qa.craftsmanship.katas.london.tdd.TransactionRepository;
+import com.alithya.qa.craftsmanship.katas.london.tdd.unit.domain.Account;
 import io.cucumber.java.Before;
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import lombok.SneakyThrows;
-import org.mockito.InOrder;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
 import java.io.PrintStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import static com.alithya.qa.craftsmanship.katas.london.tdd.Amount.amountOf;
 public class BankKataStepDefs {
 
     @Mock
@@ -32,52 +20,41 @@ public class BankKataStepDefs {
     //private methods
     @SneakyThrows
     private void createNewDeposit(Integer amount, Integer day, Integer month, Integer year) {
-        DateFormat DateFormatter
-                = new SimpleDateFormat("dd/MM/yyyy");
-        Date depositDate = DateFormatter.parse( day.toString() + "/" + month.toString() + "/" + year.toString());
-        Amount depositAmount = amountOf(amount);
-        this.sut.deposit(depositAmount,depositDate);
+        throw new PendingException();
     }
     @SneakyThrows
     private void createNewWithdrawal(Integer amount, Integer day, Integer month, Integer year) {
-        DateFormat DateFormatter
-                = new SimpleDateFormat("dd/MM/yyyy");
-        Date withdrawalDate = DateFormatter.parse( day.toString() + "/" + month.toString() + "/" + year.toString());
-        Amount withdrawalAmount = amountOf(amount);
-        this.sut.withdrawal(withdrawalAmount,withdrawalDate);
+        throw new PendingException();
     }
 
     //public methods
     @Before
     public void setup(){
-        MockitoAnnotations.initMocks(this);
-        TransactionRepository transactionRepository = new TransactionRepository();
-        Statement statement = new Statement();
-        sut = new Account("",statement,transactionRepository);
+        throw new PendingException();
     }
 
     @Given("{string} makes a deposit of {int} on {int}-{int}-{int}")
     public void makes_a_deposit_of_on(String customerName, Integer amount, Integer day, Integer month, Integer year) {
-        this.sut.accountID = customerName;
-        createNewDeposit(amount, day, month, year);
+        throw new PendingException();
     }
 
 
 
     @And("a deposit of {int} on {int}-{int}-{int}")
     public void a_deposit_of_on(Integer amount, Integer day, Integer month, Integer year) {
-        createNewDeposit(amount, day, month, year);
+        throw new PendingException();
     }
     @And("a withdrawal of {int} on {int}-{int}-{int}")
     public void a_withdrawal_of_on(Integer amount, Integer day, Integer month, Integer year) {
-        createNewWithdrawal(amount, day, month, year);
+        throw new PendingException();
     }
 
 
 
     @When("he prints his bank statement")
     public void he_prints_his_bank_statement() {
-        this.sut.printStatement(mockPrintStream);
+
+        throw new PendingException();
     }
     @Then("he would see")
     public void he_would_see(io.cucumber.datatable.DataTable dataTable) {
@@ -88,14 +65,7 @@ public class BankKataStepDefs {
         // Double, Byte, Short, Long, BigInteger or BigDecimal.
         //
         // For other transformations you can register a DataTableType.
-        List<Map<String, String>> mappedList = dataTable.asMaps(String.class, String.class);
-
-        InOrder inOrder = Mockito.inOrder(mockPrintStream);
-        inOrder.verify(mockPrintStream).println("| date       | credit  | debit    | balance |");
-        inOrder.verify(mockPrintStream).println("| 14/01/2012 |         | 500.00 | 2500.00 |");
-        //inOrder.verify(mockPrintStream).println("| 13/01/2012 | 2000.00 |        | 3000.00 |");
-        //inOrder.verify(mockPrintStream).println("| 10/01/2012 | 1000.00 |        | 1000.00 |");
-
+        throw new PendingException();
 
     }
 
---
+++ src/test/java/com/alithya/qa/craftsmanship/katas/london/tdd/unit/domain/Account.java
@@ -0,0 +1,26 @@
+package com.alithya.qa.craftsmanship.katas.london.tdd.unit.domain;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class Account {
+
+//    private static final String SYSTEM_DATE = "01/04/2004";
+//    private static final List<Transaction> TRANSACTION_LIST = new ArrayList<>();
+//
+//    @Mock Transactions transactions;
+//    @Mock
+//    Clock clock;
+//    @Mock StatementPrinter statementPrinter;
+//
+//    private Account account;
+//
+//    @Before
+//    public void initialise() {
+//        given(clock.dateAsString()).willReturn(SYSTEM_DATE);
+//        account = new Account(clock, transactions, statementPrinter);
+//    }
+
+
+}
